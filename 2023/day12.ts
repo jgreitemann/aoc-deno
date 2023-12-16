910b3dfa935ff044b030f51a9ced109017add38c
Jonas Greitemann <jgreitemann@gmail.com>
2023-12-16 18:03:38 +0100

    🧹 Clean-up dead ends

--- 2023/day12.ts
+++ 2023/day12.ts
@@ -1,6 +1,6 @@
 import { Hasher, HashMap } from "https://deno.land/x/rimbu@1.0.2/hashed/mod.ts";
 import { Solution } from "../solution.ts";
-import { iter, product, sum, zip } from "../utils/iter.ts";
+import { sum } from "../utils/iter.ts";
 
 export default <Solution<Report[]>> {
   parse: parseReports,
@@ -10,11 +10,10 @@ export default <Solution<Report[]>> {
     );
   },
   part2(reports: Report[]): number {
-    const cache = FlatHashMap.builder<Report, number>();
     return sum(
       reports.map((report) => {
         const { pattern, runs } = unfold(report);
-        return determineCombinations(pattern, runs, cache);
+        return determineCombinations(pattern, runs);
       }),
     );
   },
@@ -40,106 +39,9 @@ export function unfold(report: Report, repetitions = 5): Report {
   };
 }
 
-export function reduceReport({ pattern, runs }: Report): Report {
-  while (pattern.length > 0) {
-    if (pattern[0] === ".") {
-      pattern = pattern.replace(/^\.+/, "");
-    }
-    if (pattern[0] === "#") {
-      if (runs.length === 0) {
-        return { pattern: "#", runs: [] };
-      }
-      const [run, ...restRuns] = runs;
-      if (
-        pattern.length >= run &&
-        pattern.substring(1, run).indexOf(".") === -1 &&
-        (pattern.length === run || pattern[run] !== "#")
-      ) {
-        pattern = pattern.substring(run + 1);
-        runs = restRuns;
-      } else {
-        return { pattern: "#", runs: [] };
-      }
-    } else {
-      break;
-    }
-  }
-
-  while (pattern.length > 0) {
-    if (pattern[pattern.length - 1] === ".") {
-      pattern = pattern.replace(/\.+$/, "");
-    }
-    if (pattern[pattern.length - 1] === "#") {
-      if (runs.length === 0) {
-        return { pattern: "#", runs: [] };
-      }
-      const run = runs[runs.length - 1];
-      if (
-        pattern.length >= run &&
-        pattern.substring(pattern.length - 1 - run).indexOf(".") === -1 &&
-        (pattern.length === run || pattern[pattern.length - 1 - run] !== "#")
-      ) {
-        pattern = pattern.substring(0, pattern.length - 1 - run);
-        runs = runs.slice(0, runs.length - 1);
-      } else {
-        return { pattern: "#", runs: [] };
-      }
-    } else {
-      break;
-    }
-  }
-
-  return { pattern, runs };
-}
-
-function applyPattern(pattern: string, replacements: string[]): string {
-  return replacements.reduce(
-    (pattern, replacement) => pattern.replace("?", replacement),
-    pattern,
-  );
-}
-
-function* arrangements(n: number, k: number): Generator<string[]> {
-  if (n < k || k < 0) {
-    return;
-  } else if (n === k) {
-    yield Array(n).fill("#");
-  } else if (k === 0) {
-    yield Array(n).fill(".");
-  } else {
-    for (const a of arrangements(n - 1, k)) {
-      yield [".", ...a];
-    }
-    for (const a of arrangements(n - 1, k - 1)) {
-      yield ["#", ...a];
-    }
-  }
-}
-
-function isCompatible(pattern: string, runs: number[]): boolean {
-  const tokens = pattern.split(/\.+/).map((s) => s.length).filter((l) => l > 0);
-  if (tokens.length !== runs.length) {
-    return false;
-  }
-  return tokens.every((t, i) => t === runs[i]);
-}
-
-export function bruteForceCombinations(
+function shiftingRunCombinations(
   pattern: string,
   runs: number[],
-): number {
-  const hashes = pattern.split("").filter((c) => c === "#").length;
-  const blanks = pattern.split("").filter((c) => c === "?").length;
-  return iter(arrangements(blanks, sum(runs) - hashes))
-    .map((replacements) => applyPattern(pattern, replacements))
-    .filter((p) => isCompatible(p, runs))
-    .count();
-}
-
-export function shiftingRunCombinations(
-  pattern: string,
-  runs: number[],
-  standalone = true,
   cache = FlatHashMap.builder<Report, number>(),
 ): number {
   function combinationsInRange(
@@ -169,7 +71,7 @@ export function shiftingRunCombinations(
       if (start > 0 && pattern[start - 1] === "#") {
         break;
       }
-      if (standalone && pattern.substring(start, start + run).includes(".")) {
+      if (pattern.substring(start, start + run).includes(".")) {
         continue;
       }
       if (pattern[start + run] === "#") {
@@ -187,86 +89,6 @@ export function shiftingRunCombinations(
   return combinationsInRange(0, pattern.length, runs);
 }
 
-export function runDistributions(
-  groups: string[],
-  runs: number[],
-): number[][][] {
-  const maxRunsByGroup = groups
-    .map((group) =>
-      iter(group)
-        .scan(
-          (last, elem) => elem === "?" ? (last === "." ? "#" : ".") : elem,
-          ".",
-        )
-        .collect() // TODO: Use case for Iter.split
-        .join("")
-        .split(".")
-        .filter((s) => s.length > 0).length
-    );
-
-  const lengthByGroup = groups.map((group) => group.length);
-
-  function* generateDistributions(
-    maxRunsByGroup: number[],
-    lengthByGroup: number[],
-    runs: number[],
-  ): Generator<number[][]> {
-    if (sum(maxRunsByGroup) < runs.length) {
-      return;
-    }
-    const [max, ...maxRest] = maxRunsByGroup;
-    const [length, ...lengthRest] = lengthByGroup;
-    if (maxRest.length === 0) {
-      yield [runs];
-    } else {
-      for (let n = 0; n <= Math.min(max, runs.length); ++n) {
-        const slice = runs.slice(0, n);
-        if (2 * sum(slice) + slice.length > 2 * length + 1) {
-          break;
-        }
-        for (
-          const rest of generateDistributions(
-            maxRest,
-            lengthRest,
-            runs.slice(n),
-          )
-        ) {
-          yield [slice, ...rest];
-        }
-      }
-    }
-  }
-
-  return [...generateDistributions(maxRunsByGroup, lengthByGroup, runs)];
-}
-
-function tryAllQuestionMarkCombinations(
-  pattern: string,
-  runs: number[],
-): number | undefined {
-  for (let i = 0; i < pattern.length; ++i) {
-    if (pattern[i] !== "?") {
-      return undefined;
-    }
-  }
-
-  // Fast path: Pattern is just question marks so the number of combinations
-  //            can be determined analytically from combinatorics:
-  return binom(pattern.length - sum(runs) + 1, runs.length);
-}
-
-function binom(n: number, k: number): number {
-  if (k < 0 || k > n) {
-    return 0;
-  }
-  k = Math.min(k, n - k);
-  let c = 1;
-  for (let i = 0; i < k; ++i) {
-    c = c * (n - i) / (i + 1);
-  }
-  return c;
-}
-
 const FlatHashMap = HashMap.createContext({
   hasher: Hasher.anyFlatHasher(),
 });
@@ -274,22 +96,7 @@ const FlatHashMap = HashMap.createContext({
 export function determineCombinations(
   pattern: string,
   runs: number[],
-  cache = FlatHashMap.builder<Report, number>(),
 ): number {
-  const groups = pattern.split(/\.+/).filter((s) => s.length > 0);
-
-  return sum(
-    iter(runDistributions(groups, runs))
-      .map((distribution) =>
-        product(
-          zip(groups, distribution)
-            .map(([pattern, runs]) => ({ pattern, runs }))
-            .map(reduceReport)
-            .map(({ pattern, runs }) =>
-              tryAllQuestionMarkCombinations(pattern, runs) ??
-                shiftingRunCombinations(pattern, runs, false, cache)
-            ),
-        )
-      ),
-  );
+  const cache = FlatHashMap.builder<Report, number>();
+  return shiftingRunCombinations(pattern, runs, cache);
 }
--- 2023/day12_test.ts
+++ 2023/day12_test.ts
@@ -1,12 +1,8 @@
 import { assertEquals } from "https://deno.land/std@0.201.0/assert/mod.ts";
 import {
-  bruteForceCombinations,
   determineCombinations,
   parseReports,
-  reduceReport,
   Report,
-  runDistributions,
-  shiftingRunCombinations,
   unfold,
 } from "./day12.ts";
 
@@ -49,135 +45,6 @@ Deno.test("Report can be unfolded", () => {
     runs: [1, 1, 3, 1, 1, 3, 1, 1, 3, 1, 1, 3, 1, 1, 3],
   });
 });
-
-Deno.test("Reduce reports with leading/trailing dots or hashes", () => {
-  assertEquals(reduceReport({ pattern: "#????", runs: [1, 2] }), {
-    pattern: "???",
-    runs: [2],
-  });
-  assertEquals(reduceReport({ pattern: "#????", runs: [2, 1] }), {
-    pattern: "??",
-    runs: [1],
-  });
-  assertEquals(reduceReport({ pattern: "###??", runs: [2, 1] }), {
-    pattern: "#",
-    runs: [],
-  });
-  assertEquals(reduceReport({ pattern: "????#", runs: [2, 1] }), {
-    pattern: "???",
-    runs: [2],
-  });
-  assertEquals(reduceReport({ pattern: "????#", runs: [1, 2] }), {
-    pattern: "??",
-    runs: [1],
-  });
-  assertEquals(reduceReport({ pattern: "#???#", runs: [1, 2] }), {
-    pattern: "",
-    runs: [],
-  });
-  assertEquals(reduceReport({ pattern: "#????#", runs: [1, 2] }), {
-    pattern: "?",
-    runs: [],
-  });
-  assertEquals(
-    reduceReport({
-      pattern: "#?#?#???#???#?#??#",
-      runs: [1, 1, 1, 3, 1, 1, 2],
-    }),
-    { pattern: "??#??", runs: [3] },
-  );
-});
-
-Deno.test("Distribute runs over groups", () => {
-  assertEquals(
-    runDistributions(["#?#", "?#"], [1, 1, 2]),
-    [[[1, 1], [2]]],
-  );
-  assertEquals(
-    runDistributions(["?####?????", "#???"], [5, 1, 3]),
-    [
-      [[5], [1, 3]],
-      [[5, 1], [3]],
-      [[5, 1, 3], []],
-    ],
-  );
-  assertEquals(
-    runDistributions(["??", "?????", "?"], [3, 1]),
-    [
-      [[], [3], [1]],
-      [[], [3, 1], []],
-    ],
-  );
-});
-
-[
-  { report: { pattern: "???", runs: [1, 1] }, combinations: 1 },
-  { report: { pattern: "???", runs: [1] }, combinations: 3 },
-  { report: { pattern: "???", runs: [2] }, combinations: 2 },
-  { report: { pattern: "???", runs: [3] }, combinations: 1 },
-  { report: { pattern: "???", runs: [1, 2] }, combinations: 0 },
-  { report: { pattern: "?#?", runs: [1] }, combinations: 1 },
-  { report: { pattern: "?#?", runs: [2] }, combinations: 2 },
-  { report: { pattern: "?#?", runs: [3] }, combinations: 1 },
-  { report: { pattern: "#??", runs: [1] }, combinations: 1 },
-  { report: { pattern: "#??", runs: [2] }, combinations: 1 },
-  { report: { pattern: "#??", runs: [3] }, combinations: 1 },
-  { report: { pattern: "?.?", runs: [1] }, combinations: 2 },
-  { report: { pattern: "?.?", runs: [2] }, combinations: 0 },
-  { report: { pattern: "?.?", runs: [3] }, combinations: 0 },
-  { report: { pattern: ".??", runs: [1] }, combinations: 2 },
-  { report: { pattern: ".??", runs: [2] }, combinations: 1 },
-  { report: ALL_QUESTION_MARK_REPORTS[0], combinations: 1 },
-  { report: ALL_QUESTION_MARK_REPORTS[1], combinations: 3 },
-  { report: ALL_QUESTION_MARK_REPORTS[2], combinations: 3 },
-  { report: ALL_QUESTION_MARK_REPORTS[3], combinations: 6 },
-  { report: ALL_QUESTION_MARK_REPORTS[4], combinations: 15 },
-  { report: ALL_QUESTION_MARK_REPORTS[5], combinations: 10 },
-  { report: ALL_QUESTION_MARK_REPORTS[6], combinations: 5 },
-  { report: ALL_QUESTION_MARK_REPORTS[7], combinations: 70 },
-  { report: ALL_QUESTION_MARK_REPORTS[8], combinations: 1 },
-  { report: ALL_QUESTION_MARK_REPORTS[9], combinations: 0 },
-].forEach(({ report: { pattern, runs }, combinations }) =>
-  Deno.test(`Simple reports can be brute-forced: ${pattern} ~ [${runs}] => ${combinations}`, () => {
-    assertEquals(bruteForceCombinations(pattern, runs), combinations);
-  })
-);
-
-[
-  { report: EXAMPLE_REPORT[0], combinations: 1 },
-  { report: EXAMPLE_REPORT[1], combinations: 4 },
-  { report: EXAMPLE_REPORT[2], combinations: 1 },
-  { report: EXAMPLE_REPORT[3], combinations: 1 },
-  { report: EXAMPLE_REPORT[4], combinations: 4 },
-  { report: EXAMPLE_REPORT[5], combinations: 10 },
-  { report: { pattern: "??.?????.?", runs: [3, 1] }, combinations: 4 },
-  {
-    report: { pattern: "#?????????#????", runs: [1, 3, 2, 2] },
-    combinations: 17,
-  },
-  {
-    report: { pattern: "?#??#..#?#???#??#.", runs: [5, 1, 1, 4] },
-    combinations: 1,
-  },
-  {
-    report: { pattern: "?????.????#?#?", runs: [1, 1, 2, 4] },
-    combinations: 18,
-  },
-  { report: ALL_QUESTION_MARK_REPORTS[0], combinations: 1 },
-  { report: ALL_QUESTION_MARK_REPORTS[1], combinations: 3 },
-  { report: ALL_QUESTION_MARK_REPORTS[2], combinations: 3 },
-  { report: ALL_QUESTION_MARK_REPORTS[3], combinations: 6 },
-  { report: ALL_QUESTION_MARK_REPORTS[4], combinations: 15 },
-  { report: ALL_QUESTION_MARK_REPORTS[5], combinations: 10 },
-  { report: ALL_QUESTION_MARK_REPORTS[6], combinations: 5 },
-  { report: ALL_QUESTION_MARK_REPORTS[7], combinations: 70 },
-  { report: ALL_QUESTION_MARK_REPORTS[8], combinations: 1 },
-  { report: ALL_QUESTION_MARK_REPORTS[9], combinations: 0 },
-].forEach(({ report: { pattern, runs }, combinations }) =>
-  Deno.test(`Example report using shiftingRuns: ${pattern} ~ [${runs}] => ${combinations}`, () => {
-    assertEquals(shiftingRunCombinations(pattern, runs), combinations);
-  })
-);
 
 [
   { report: EXAMPLE_REPORT[0], combinations: 1 },
@@ -237,6 +104,10 @@ Deno.test("Distribute runs over groups", () => {
   { report: ALL_QUESTION_MARK_REPORTS[5], combinations: 3268760 },
   { report: ALL_QUESTION_MARK_REPORTS[6], combinations: 53130 },
   { report: ALL_QUESTION_MARK_REPORTS[7], combinations: 137846528820 },
+  {
+    report: { pattern: "?.?.?#?##????.?.?#", runs: [1, 1, 3, 2, 1] },
+    combinations: 512,
+  },
 ].forEach(({ report: { pattern, runs }, combinations }) =>
   Deno.test(`Example report unfolded: ${pattern} ~ [${runs}] => ${combinations}`, () => {
     const unfolded = unfold({ pattern, runs });
